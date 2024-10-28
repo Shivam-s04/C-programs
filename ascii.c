1d968db378a079eaa0109348a4b6fa481b40c280
Shivam Sunderam <shivamsunderam04@gmail.com>
2024-10-28 11:57:59 +0530

    dynamic allocation
    dynamic allocation of matrix

--- ascii.c
+++ ascii.c
@@ -1,8 +1,17 @@
 // learning ascii
 #include <stdio.h>
+
+void printASCII();
 void main(){
     char a = '0';
     char b = '1';
-    printf("%d ",a+b);
-    printf("%c",a+b);
+    // printf("%d ",a+b);
+    // printf("%c",a+b);
+     printASCII();
+}
+
+void printASCII(){
+    for(int i = 32; i< 123 ; i++){
+        printf("%c ",i);
+    }
 }
--- matrix.c
+++ matrix.c
@@ -1,19 +1,44 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<string.h>
+#include<stdlib.h>
 
-void matrix_add(int m1[][],int m2[][], int row, int col);
-
+void matrix_add(int m1[][3],int m2[][3], int row, int col);
+void matrix_mul(int m1[][3],int m2[][3],int m3[][3] , int row, int col);
+void get_data(int *arr, int size);
 
 void main(){
-    int m1[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
-    int m2[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
-    matrix_add(m1,m2,3,3);
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", m1[i][j]);
-        }
-        printf("\n");
+
+    //dynamic memory allocation
+    int size ;
+    scanf("%d",&size );
+    int *arr  = (int*)malloc(size*sizeof(int));
+     get_data( arr, size);
+    
+    //print
+    for(int i=0 ; i<size ; i++){
+        printf("%d " , arr[i]);
+    }
+
+
+
+
+
+
+    // prev code;
+    // matrix_mul(m1,m2,m3,3,3);
+    // for (int i = 0; i < 3; i++) {
+    //     for (int j = 0; j < 3; j++) {
+            // printf("%d ", m3[i][j]);
+    // //     }
+    //     printf("\n");
+    // }
+}
+
+
+void get_data(int *arr, int size){
+    for(int i = 0 ; i<size ; i++){
+        arr[i] = i+1;
     }
 }
 
@@ -24,3 +49,15 @@ void matrix_add(int m[][3], int arr[][3], int row, int col) {
         }
     }
 }
+
+void matrix_mul(int m1[][3],int m2[][3],int m3[][3] , int row, int col){
+    for(int i = 1 ; i<=row ; i++){
+        for(int j = 1 ; j<=row ; j++){
+            for(int k = 1 ; k<=row ; k++){
+                m3[i][j] = (m1[i][k]*m2[k][j]);
+                        
+                }
+            }
+    }
+}
+

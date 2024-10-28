95d7ddb5b47c693a2233667fc439b1fdde4ba8eb
Shivam Sunderam <shivamsunderam04@gmail.com>
2024-10-28 13:15:45 +0530

    update

--- matrix.c
+++ matrix.c
@@ -6,34 +6,64 @@
 void matrix_add(int m1[][3],int m2[][3], int row, int col);
 void matrix_mul(int m1[][3],int m2[][3],int m3[][3] , int row, int col);
 void get_data(int *arr, int size);
+void merging (int arr1[],int arr2[],int arr3[],int size,int size1,int size2);
 
 void main(){
 
+    int arr1[] = {1,2,14};
+    int arr2[] = {8,9,10,11,12,13}; 
+    
+    
+    int size1  = sizeof(arr1)/sizeof(arr1[0]);
+    int size2  = sizeof(arr2)/sizeof(arr2[0]);
+    int size  = size1 + size2;
+
+    int res[size];
+     merging (arr1,arr2,res,size,size1,size2);
+     for(int i = 0; i<size; i++){
+        printf("%d ",res[i]);
+     }
+     
+
+    
+
+
     //dynamic memory allocation
-    int size ;
-    scanf("%d",&size );
-    int *arr  = (int*)malloc(size*sizeof(int));
-     get_data( arr, size);
+    // int *arr  = (int*)malloc(size*sizeof(int));
+    //  get_data( arr, size);
     
     //print
-    for(int i=0 ; i<size ; i++){
-        printf("%d " , arr[i]);
-    }
-
-
-
-
-
-
-    // prev code;
-    // matrix_mul(m1,m2,m3,3,3);
-    // for (int i = 0; i < 3; i++) {
-    //     for (int j = 0; j < 3; j++) {
-            // printf("%d ", m3[i][j]);
-    // //     }
+    // for(int i=0 ; i<size ; i++){
+    //     printf("%d\t" , arr[i]);
+    //     if(arr[i]%10 == 0)
     //     printf("\n");
     // }
 }
+    void merging (int arr1[],int arr2[],int res[],int size,int size1,int size2){
+        int i=0,j=0,k=0;
+        // int size1 = strlen(arr1);
+        // int size2 = strlen(arr2); since in function they are pointers, strlen cant be used !!
+        while(i<size1 && j<size2){
+            if(arr1[i]<arr2[j]){
+                res[k++] = arr1[i++];
+            }
+            else if(arr1[i]>arr2[j]){
+                res[k++] = arr2[j++];
+            }
+            else{
+                res[k++] = arr2[j++];
+                res[k++] = arr1[i++];
+            }
+        
+        }
+        while(i<size1){
+            res[k++] = arr1[i++];
+        }
+        while(j<size2){
+            res[k++] = arr2[j++];
+        }
+    }
+
 
 
 void get_data(int *arr, int size){
